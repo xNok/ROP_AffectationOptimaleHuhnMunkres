07841890abe309c599aa87427fa306f1c60c3aef
Alexandre Couedelo <alexandre.couedelo@mines-ales.org>
2016-02-26 20:22:54 +0100

    réalisation de f_GrapheEgalite

--- f_GrapheEgalite.m
+++ f_GrapheEgalite.m
@@ -1,19 +1,27 @@
 function [ GL ] = f_GrapheEgalite( E, L )
 %F_GRAPHEEGALITE create d'un graphe d'égalité 
 %   GL = (P union T, A), (x,y) in A <=> L(x)+L(y)=E(x,y)
-%   
-
-% Initialisation
-GL.NbVertices = 0;   % Initially empty graph
-GL.NbEdges = 0;      % Initially empty graph
-GL.Orientation = 0;  % [0] - Non directed; 1 - directed; 2 - mixte
-GL.AdjList = [];     % Initially empty graph
 
 % Matrice d'égalité
 ML = E==L.x*ones(1,10)+ones(10,1)*L.y
-max(ML)
+
+% Initialisation
+GL.NbVertices = 2*size(E,1);   % Initially empty graph
+GL.NbEdges = nnz(ML==1);      % Initially empty graph
+GL.Orientation = 1;  % [0] - Non directed; 1 - directed; 2 - mixte
+GL.AdjList = [];     % Initially empty graph
+
 GL.AdjList = cell(1,GL.NbVertices);
+for k = 1 : GL.NbVertices, GL.AdjList{k} = []; end
 
-
+for i = 1:GL.NbVertices
+ nProcesseur = GL.NbVertices/2;
+ 
+ if i <  nProcesseur+1
+    i_vertices = find(ML(i,:)==1);
+ else
+    i_vertices = find(ML(:,i-nProcesseur)==1);
+ end
+    GL.AdjList{i} = [i_vertices; E(i_vertices)];
 end
 
