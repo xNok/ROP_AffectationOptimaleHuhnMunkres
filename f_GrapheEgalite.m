5da94661cb86cbbf2b57f5841bcdef4a52c3997e
Alexandre Couedelo <alexandre.couedelo@mines-ales.org>
2016-02-26 20:37:13 +0100

    correction on f_grapheEgalite

--- f_GrapheEgalite.m
+++ f_GrapheEgalite.m
@@ -3,7 +3,7 @@ function [ GL ] = f_GrapheEgalite( E, L )
 %   GL = (P union T, A), (x,y) in A <=> L(x)+L(y)=E(x,y)
 
 % Matrice d'égalité
-ML = E==L.x*ones(1,10)+ones(10,1)*L.y
+ML = E==L.x*ones(1,size(E,1)+ones(size(E,1,1)*L.y
 
 % Initialisation
 GL.NbVertices = 2*size(E,1);   % Initially empty graph
@@ -19,9 +19,11 @@ for i = 1:GL.NbVertices
  
  if i <  nProcesseur+1
     i_vertices = find(ML(i,:)==1);
+    GL.AdjList{i} = [i_vertices; E(i, i_vertices)];
  else
     i_vertices = find(ML(:,i-nProcesseur)==1);
+    GL.AdjList{i} = [i_vertices; E(i_vertices, i-i_vertices)];
  end
-    GL.AdjList{i} = [i_vertices; E(i_vertices)];
+    
 end
 
