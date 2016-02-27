f35e01c4b10cf39a1309acf808e6b01c703fb23c
Alexandre Couedelo <alexandre.couedelo@mines-ales.org>
2016-02-27 20:32:44 +0100

    wip

--- f_CouplageMax.m
+++ f_CouplageMax.m
@@ -1,4 +1,4 @@
-function [C AB] = f_CouplageMax( GL )
+function [C L] = f_CouplageMax( GL )
 %F_COUPLAGEMAX donne le couplage max a partir de l'algorithme
 %   FlotMaxEdmondsKarp
 
@@ -7,7 +7,7 @@ GB = f_GBiparti2Reseau(GL) %Convertie GL en ajoutant un puit et une source
 [FMax, L, Cmin, Smin] = f_FlotMaxEdmondsKarp(GB, 1, GB.NbVertices, 2) %determine le flotMax
 
 % Construit le vercteur C
-C = [L((GB.NbVertices/2):(size(L,1)-(GB.NbVertices/2)),1)-1 L((GB.NbVertices/2):(size(L,1)-(GB.NbVertices/2)),2)]
+C = [L((GB.NbVertices/2):(size(L,1)-(GB.NbVertices/2)),1)-1 L((GB.NbVertices/2):(size(L,1)-(GB.NbVertices/2)),2)-1]
 
 end
 
---
+++ f_ModifierEtiquette.m
@@ -0,0 +1,10 @@
+function L = f_ModifierEtiquette(GL, C, L)
+%F_MODIFIERETIQUETTE Summary of this function goes here
+%   Detailed explanation goes here
+
+r = (G.NbVertices/2)+find(ismember([(G.NbVertices/2+1):G.NbVertices], C)==0)
+
+
+
+end
+
