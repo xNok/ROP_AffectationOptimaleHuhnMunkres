00a58730e5f5ef806fd0be47bed244b69aeed2a0
Alexandre Couedelo <alexandre.couedelo@mines-ales.org>
2016-02-27 13:17:19 +0100

    Ajout d'un algorithme externe vérification

--- main_Project_taskManagement.m
+++ main_Project_taskManagement.m
@@ -6,7 +6,7 @@
 % Connaissant A(i,j) la perfommance de réalisation de la tâche Ti par le processeur la Pj
 % On cherche l'affectation conduissant à la performance total maximum 
 %
-% Resources : 
+% Resources :
 % Fondamentaux de Recherche Opérationnelle, EMA, Stephane Janaqi
 % cours.ensem.inpl-nancy.fr/cours-dm/graphes/Graphes.pdf
 % https://en.wikipedia.org/wiki/Hungarian_algorithm
@@ -15,21 +15,39 @@
 % La graphe G est un graphe biparti complet, il peut donc être définit de
 % manière unique par la matrice des performances E
 
+%--------------------------------------------------
 % 0. Nettoyage
+%--------------------------------------------------
 clear all, close all, clc
 
+
+%--------------------------------------------------
 % 1. Laod Matrix
+%--------------------------------------------------
 path = 'D:\03-Ecole\2A\ROP\Projet_AC_FI\';
 filename = strcat(path , 'M10.txt');
 E = importdata(filename);
 n = size(E);
 
+%--------------------------------------------------
 % 2. Algorythm
+%--------------------------------------------------
 L = f_EtiquetesInitialsesFaisables(E);
 GL = f_GrapheEgalite(E,L);
 C = f_CouplageMax(GL);
 
+%--------------------------------------------------
+% Verification avec un algoritme de professionelle
+%--------------------------------------------------
 
+% Transformation de la matrice des performance en matrice de coût
+B=E;
+for i=1:10 
+    B(i,:) = - ( E(i,:) - max(E(i,:)));
+end
+
+
+assignment = munkres(B)
 
 
 
