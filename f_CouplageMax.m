be076243b7933c434286436ccb3e5a94abb719c6
Alexandre Couedelo <alexandre.couedelo@mines-ales.org>
2016-02-27 17:36:41 +0100

    add CouplageMax
    modification de flavien

--- f_CouplageMax.m
+++ f_CouplageMax.m
@@ -1,12 +1,13 @@
-function [ C ] = f_CouplageMax( GL )
+function [C AB] = f_CouplageMax( GL )
 %F_COUPLAGEMAX donne le couplage max a partir de l'algorithme
 %   FlotMaxEdmondsKarp
 
 
 GB = f_GBiparti2Reseau(GL) %Convertie GL en ajoutant un puit et une source
-[FMax, L, Cmin, Smin] = f_FlotMaxEdmondsKarp(GB, 1, GB.NbVertices, 2)
+[FMax, L, Cmin, Smin] = f_FlotMaxEdmondsKarp(GB, 1, GB.NbVertices, 2) %determine le flotMax
 
-C = []
+% Construit le vercteur C
+C = [L((GB.NbVertices/2):(size(L,1)-(GB.NbVertices/2)),1)-1 L((GB.NbVertices/2):(size(L,1)-(GB.NbVertices/2)),2)]
 
 end
 
